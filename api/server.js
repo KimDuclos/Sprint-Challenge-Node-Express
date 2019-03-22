f0e644c9722757d445ae6b29e94c2a34671a071b
Kim Duclos <kimduclos@gmail.com>
2019-03-22 09:15:11 -0600

    initial commit

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json()); 
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Api is running on port 4000' });
+});
+
+module.exports = server;
